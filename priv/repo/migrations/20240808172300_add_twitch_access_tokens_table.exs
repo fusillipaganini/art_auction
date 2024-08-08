781f09748532818e96da20c5d5df5fa1e99fb890
Fusilli Paganini <fusillipaganini@gmail.com>
2024-08-08 17:27:28 -0300

    Almost forgot TwitchAccessToken migration

---
+++ priv/repo/migrations/20240808172300_add_twitch_access_tokens_table.exs
@@ -0,0 +1,19 @@
+defmodule ArtAuction.Migrations.AddTwitchAccessTokensTable do
+  use Ecto.Migration
+
+  def change do
+    create table("twitch_access_tokens") do
+      add :access_token, :string, null: false
+      add :refresh_token, :string
+      add :twitch_user_id, :string
+      add :twitch_username, :string
+      add :expires_at, :utc_datetime, null: false
+      add :validate_at, :utc_datetime
+      add :status, :smallint
+
+      add :account_id, references("accounts", on_delete: :delete_all, on_update: :restrict)
+
+      timestamps(type: :utc_datetime)
+    end
+  end
+end
